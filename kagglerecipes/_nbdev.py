cfda2d09ff64d602794bbd8e73bbdb24180cac42
Ayush Thakur <mein2work@gmail.com>
2021-09-03 03:37:11 +0530

    wandb_viz import plot_basic_chart

--- kagglerecipes/_nbdev.py
+++ kagglerecipes/_nbdev.py
@@ -16,6 +16,7 @@ index = {"TINY_DATA_PATH": "00_data.ipynb",
          "get_image_plane": "02_utils.ipynb",
          "log_df_as_tables": "03_wandb_utils.ipynb",
          "log_to_artifacts": "03_wandb_utils.ipynb",
+         "plot_basic_charts": "04_wandb_viz.ipynb",
          "log_images": "04_wandb_viz.ipynb"}
 
 modules = ["data.py",
